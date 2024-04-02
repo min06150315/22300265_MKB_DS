58cc6e3a35f5752345a514f2b1c2d218e805857a
min06150315 <min06150315@gmail.com>
2024-04-02 12:30:30 +0900

    DS008 Complete

---
+++ DS Lab1/DS008.cpp
@@ -0,0 +1,41 @@
+// 8. 문장을 숫자로 변환하기 (버전2)
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string sentence;
+    int result = 0;
+
+    char alpha[] = {'I', 'V', 'X', 'L', 'C', 'D', 'M'};
+    int alphaNum[] = {1, 5, 10, 50, 100, 500, 1000};
+
+    cout << "Enter the sentence: ";
+    getline(cin, sentence);
+
+    int len = sentence.length();
+
+    for (int i = 0; i < len; i++){
+        for (int j = 0; j < 7; j++){
+            if (sentence[i] == alpha[j]){
+                result = result + alphaNum[j];
+                // Rule 1
+                if ((sentence[i] == 'I' && sentence[i + 1] == 'V') || (sentence[i] == 'I' && sentence[i + 1] == 'X')){
+                    result = result - alphaNum[j] * 2;
+                }
+                // Rule 2
+                if ((sentence[i] == 'X' && sentence[i + 1] == 'L') || (sentence[i] == 'X' && sentence[i + 1] == 'C')){
+                    result = result - alphaNum[j] * 2;
+                }
+                // Rule 3
+                if ((sentence[i] == 'C' && sentence[i + 1] == 'D') || (sentence[i] == 'C' && sentence[i + 1] == 'M')){
+                    result = result - alphaNum[j] * 2;
+                }
+            }
+        }
+    }
+    cout << result << endl;
+
+    return 0;
+}
