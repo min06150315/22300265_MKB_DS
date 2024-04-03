f7f9cd0ff4a16e6fac41000d2b8d0b88c448f59c
min06150315 <min06150315@gmail.com>
2024-04-03 21:57:42 +0900

    DS lab1

--- DS Lab1/DS008.cpp
+++ DS Lab1/DS008.cpp
@@ -11,11 +11,14 @@ int main()
     char alpha[] = {'I', 'V', 'X', 'L', 'C', 'D', 'M'};
     int alphaNum[] = {1, 5, 10, 50, 100, 500, 1000};
 
+    // 유저에게 문장 입력 받기
     cout << "Enter the sentence: ";
     getline(cin, sentence);
 
+    // 문장의 길이 구하기
     int len = sentence.length();
-
+    
+    // 문장에 맞는 수 찾아내서 계산하기
     for (int i = 0; i < len; i++){
         for (int j = 0; j < 7; j++){
             if (sentence[i] == alpha[j]){
@@ -35,6 +38,7 @@ int main()
             }
         }
     }
+    // 결과 출력
     cout << result << endl;
 
     return 0;
