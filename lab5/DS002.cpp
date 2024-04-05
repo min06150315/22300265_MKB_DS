8994d9ad73755015da8bfe49aa656f81eb7166fd
min06150315 <min06150315@gmail.com>
2024-04-05 16:42:53 +0900

    Update DS002.cpp

--- lab5/DS002.cpp
+++ lab5/DS002.cpp
@@ -3,19 +3,24 @@
 #include <cmath>
 using namespace std;
 
+double calculateDistance(int x1, int y1, int x2, int y2);
+
 int main()
 {
     double x1, y1;
     double x2, y2;
-    double result;
 
     cout << "Enter the first coordinate (x1, y1): ";
     cin >> x1 >> y1;
     cout << "Enter the second coordinate (x2, y2): ";
     cin >> x2 >> y2;
 
-    result = sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2));
-    cout << result << endl;
+    cout << calculateDistance(x1, y1, x2, y2) << endl;
 
     return 0;
+}
+
+double calculateDistance(int x1, int y1, int x2, int y2)
+{
+    return sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2));
 }
