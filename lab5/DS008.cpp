a229426069398224149c8e0b34e4b0e38a07ca54
min06150315 <min06150315@gmail.com>
2024-04-06 05:24:46 +0900

    Update DS008.cpp

--- lab5/DS008.cpp
+++ lab5/DS008.cpp
@@ -2,43 +2,45 @@
 #include <iostream>
 using namespace std;
 
+int invertNum(string str);
+
 int main()
 {
     string sentence;
-    int result = 0;
+    int result = invertNum(sentence);
+    cout << result << endl;
+    return 0;
+}
 
+int invertNum(string str)
+{
+    int result = 0;
     char alpha[] = {'I', 'V', 'X', 'L', 'C', 'D', 'M'};
     int alphaNum[] = {1, 5, 10, 50, 100, 500, 1000};
 
-    // 유저에게 문장 입력 받기
     cout << "Enter the sentence: ";
-    getline(cin, sentence);
+    getline(cin, str);
 
-    // 문장의 길이 구하기
-    int len = sentence.length();
+    int len = str.length();
     
-    // 문장에 맞는 수 찾아내서 계산하기
     for (int i = 0; i < len; i++){
         for (int j = 0; j < 7; j++){
-            if (sentence[i] == alpha[j]){
+            if (str[i] == alpha[j]){
                 result = result + alphaNum[j];
                 // Rule 1
-                if ((sentence[i] == 'I' && sentence[i + 1] == 'V') || (sentence[i] == 'I' && sentence[i + 1] == 'X')){
+                if ((str[i] == 'I' && str[i + 1] == 'V') || (str[i] == 'I' && str[i + 1] == 'X')){
                     result = result - alphaNum[j] * 2;
                 }
                 // Rule 2
-                if ((sentence[i] == 'X' && sentence[i + 1] == 'L') || (sentence[i] == 'X' && sentence[i + 1] == 'C')){
+                if ((str[i] == 'X' && str[i + 1] == 'L') || (str[i] == 'X' && str[i + 1] == 'C')){
                     result = result - alphaNum[j] * 2;
                 }
                 // Rule 3
-                if ((sentence[i] == 'C' && sentence[i + 1] == 'D') || (sentence[i] == 'C' && sentence[i + 1] == 'M')){
+                if ((str[i] == 'C' && str[i + 1] == 'D') || (str[i] == 'C' && str[i + 1] == 'M')){
                     result = result - alphaNum[j] * 2;
                 }
             }
         }
     }
-    // 결과 출력
-    cout << result << endl;
-
-    return 0;
+    return result;
 }
