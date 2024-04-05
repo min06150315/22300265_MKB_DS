0111cf86b882875123339143ecc5229c06503b63
min06150315 <min06150315@gmail.com>
2024-04-06 05:24:37 +0900

    Update DS006.cpp

--- lab5/DS006.cpp
+++ lab5/DS006.cpp
@@ -9,14 +9,21 @@ struct major{
     int grade;  // 0 ~ 8 (A+ ~ F)
 };
 
+void printGrade(major s1[3]);
+
 string GNAME[] = {"A+", "A0", "B+", "B0", "C+", "C0", "D+", "D0", "F"};
 double GNUM[] = {4.5, 4.0, 3.5, 3.0, 2.5, 2.0, 1.5, 1.0, 0.0};
+int SCORE[] = {95, 90, 85, 80, 75, 70, 65, 60};
 
 int main()
 {
     major s1[3];
+    printGrade(s1);
+    return 0;
+}
 
-    // 유저에게 입력 받기
+void printGrade(major s1[3])
+{
     cout << "Enter the first class Name: " ;
     getline(cin, s1[0].name);
     cout << "Enter the first class Credit and Score: ";
@@ -36,44 +43,26 @@ int main()
     cout << "Enter the third class Credit and Score: ";
     cin >> s1[2].credit >> s1[2].score;
 
-    // grade 찾기
     for (int i = 0; i < 3; i++){
-        if (s1[i].score >= 95){         // A+
-            s1[i].grade = 0;
-        }else if (s1[i].score >= 90){   // A0
-            s1[i].grade = 1;
-        }else if (s1[i].score >= 85){   // B+
-            s1[i].grade = 2;
-        }else if (s1[i].score >= 80){   // B0
-            s1[i].grade = 3;
-        }else if (s1[i].score >= 75){   // C+
-            s1[i].grade = 4;
-        }else if (s1[i].score >= 70){   // C0
-            s1[i].grade = 5;
-        }else if (s1[i].score >= 65){   // D+
-            s1[i].grade = 6;
-        }else if (s1[i].score >= 60){   // D0
-            s1[i].grade = 7;
-        }else {
-            s1[i].grade = 8;
+        for (int j = 0; j < 8; j++){
+            if (s1[i].score >= SCORE[j]){
+                s1[i].grade = j;
+                break;
+            }
         }
     }
 
-    // 총 학점 수, 그레이드 평균 값 구하기
     int sum = 0;
     double avg = 0;
 
-    for (int i = 0; i < 3; i++){
+    for (int i = 0; i < 3; i++)
         sum = sum + s1[i].credit;
-    }
     avg = ((GNUM[s1[0].grade] * s1[0].credit) + (GNUM[s1[1].grade] * s1[1].credit) + (GNUM[s1[2].grade] * s1[2].credit)) / sum;
-    // 결과 출력하기
+
     for (int i = 0; i < 3; i++){
         cout << s1[i].name << "(" << s1[i].credit << ")" << " " << GNAME[s1[i].grade];
         printf(" %.1f\n", GNUM[s1[i].grade]);
     }
     cout << "Total Credits " << sum << ", GPA ";
     printf("%.2f\n", avg);
-
-    return 0;
 }
