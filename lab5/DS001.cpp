7c28d6dc08cd0151a54767f3096bc196cd5cb3af
min06150315 <min06150315@gmail.com>
2024-04-05 16:30:02 +0900

    Update DS001

--- lab5/DS001.cpp
+++ lab5/DS001.cpp
@@ -1,23 +1,25 @@
 // DS001. 비만 판정하기
+// call by reference 사용하기
 #include <iostream>
 using namespace std;
+
+void checkBMI(int height, int weight);
 
 int main()
 {
     int height, weight;
-    double bmi;
     
     cout << "Enter the height (cm, kg): ";
-    cin >> height;
-    cin >> weight;
+    cin >> height >> weight;
     
-    bmi = weight / ((height * 0.01) * (height * 0.01));
-
-    if (bmi >= 25){
-        cout << "Yes" << endl;
-    }else {
-        cout << "No" << endl;
-    }
-    
+    checkBMI(height, weight);
     return 0;
+}
+
+void checkBMI(int height, int weight)
+{
+    double bmi = weight / ((height * 0.01) * (height * 0.01));
+
+    if (bmi >= 25) cout << "Yes" << endl;
+    else cout << "No" << endl;
 }
