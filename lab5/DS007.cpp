b2e889fb5bf999e1e52613c31360ba20c7cc2c0f
min06150315 <min06150315@gmail.com>
2024-04-06 05:24:42 +0900

    Update DS007.cpp

--- lab5/DS007.cpp
+++ lab5/DS007.cpp
@@ -2,31 +2,33 @@
 #include <iostream>
 using namespace std;
 
+int invertNum(string str);
+
 int main()
 {
     string sentence;
-    int result = 0;
+    int result = invertNum(sentence);
+    cout << result << endl;
+    return 0;
+}
 
+int invertNum(string str)
+{
+    int result = 0;
     char alpha[] = {'I', 'V', 'X', 'L', 'C', 'D', 'M'};
     int alphaNUm[] = {1, 5, 10, 50, 100, 500, 1000};
 
-    // 유저에게 문장 입력 받기
     cout << "Enter the sentence: ";
-    getline(cin, sentence);
+    getline(cin, str);
 
-    // 문장의 길이 구하기
-    int len = sentence.length();
+    int len = str.length();
 
-    // 문장에 맞는 수 찾아내서 계산하기
     for (int i = 0; i < len; i++){
         for (int j = 0; j < 7; j++){
-            if (sentence[i] == alpha[j]){
+            if (str[i] == alpha[j]){
                 result = result + alphaNUm[j];
             }
         }
     }
-    // 결과 출력
-    cout << result << endl;
-
-    return 0;
+    return result;
 }
