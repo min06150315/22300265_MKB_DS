9ec883d7936884eb36dce9b5f89ae26973e55904
min06150315 <min06150315@gmail.com>
2024-04-09 01:52:13 +0900

    Update ds011.cpp

--- lab6/ds011.cpp
+++ lab6/ds011.cpp
@@ -2,15 +2,24 @@
 #include <iostream>
 using namespace std;
 
+int CalculateSum(int count, int num[]);
+int CalculateMax(int count, int num[]);
+
 int main()
 {
     // 입력하고자 하는 숫자 입력 받기
     int count;
     cout << "Enter the number of numbers: ";
     cin >> count;
-
-    // 숫자 입력 받고 총합 구하기
     int num[count];
+
+    cout << CalculateSum(count, num) << " " << CalculateMax(count, num) << endl;
+    return 0;
+}
+
+// 숫자 입력 받고 총합 구하기
+int CalculateSum(int count, int num[])
+{
     int sum = 0;
     for (int i = 0; i < count; i++){
         cout << "Enter the number: ";
@@ -18,13 +27,16 @@ int main()
         sum += num[i];
     }
 
-    // 최댓값 구하기
+    return sum;
+}
+
+// 최댓값 구하기
+int CalculateMax(int count, int num[])
+{
     int max = num[0];
     for (int i = 0; i < count - 1; i++)
         if (max < num[i + 1])
             max = num[i + 1];
 
-    // 결과 출력
-    cout << "Sum: " << sum << " Max: " << max << endl;
-    return 0;
+    return max;
 }
