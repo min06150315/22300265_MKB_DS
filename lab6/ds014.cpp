901fddfa67d11e840e7a38624b38cfe61992e0f3
min06150315 <min06150315@gmail.com>
2024-04-12 11:57:22 +0900

    Update ds014.cpp

--- lab6/ds014.cpp
+++ lab6/ds014.cpp
@@ -2,13 +2,13 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
 void rtrim(string &s);
 void ltrim(string &s);
 
 int main()
 {
     string s1;
-    cout << "Enter the Sentence: ";
     getline(cin, s1);
 
     rtrim(s1);
@@ -19,14 +19,14 @@ int main()
 
 void rtrim(string &s)
 {
+    // 뒤 부터 탭과 공백이 아닌 부분 찾기
     size_t index = s.find_last_not_of(" \t");
-    if (index != string::npos)
-        s = s.substr(0, index + 1);
+    s = s.substr(0, index + 1);
 }
 
 void ltrim(string &s)
 {
+    // 앞 부터 탭과 공백이 아닌 부분 찾기
     size_t index = s.find_first_not_of(" \t");
-    if (index != string::npos)
-        s = s.substr(index);
+    s = s.substr(index);
 }
