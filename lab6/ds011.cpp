71865888a25833cfd241ca0a0594b2e00d50650a
min06150315 <min06150315@gmail.com>
2024-04-12 10:53:44 +0900

    Update ds011.cpp

--- lab6/ds011.cpp
+++ lab6/ds011.cpp
@@ -2,14 +2,12 @@
 #include <iostream>
 using namespace std;
 
-int CalculateSum(int count, int *num);
-int CalculateMax(int count, int *num);
+int CalculateSum(int count, int* num);
+int CalculateMax(int count, int* num);
 
 int main()
 {
-    // 입력하고자 하는 숫자 입력 받기
     int count;
-    cout << "Enter the number of numbers: ";
     cin >> count;
     int* num = new int[count];
 
@@ -19,20 +17,17 @@ int main()
     return 0;
 }
 
-// 숫자 입력 받고 총합 구하기
-int CalculateSum(int count, int *num)
+int CalculateSum(int count, int* num)
 {
     int sum = 0;
     for (int i = 0; i < count; i++){
-        cout << "Enter the number: ";
         cin >> num[i];
         sum += num[i];
     }
     return sum;
 }
 
-// 최댓값 구하기
-int CalculateMax(int count, int *num)
+int CalculateMax(int count, int* num)
 {
     int max = num[0];
     for (int i = 0; i < count - 1; i++)
