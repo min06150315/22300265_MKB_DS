f3d0586c997a094c63e649266bc077678b30a48f
min06150315 <min06150315@gmail.com>
2024-04-17 01:14:51 +0900

    Add ds021.cpp

---
+++ lab7/ds021.cpp
@@ -0,0 +1,42 @@
+// DS021. 성적 계산하기
+#include <iostream>
+using namespace std;
+
+class Student {
+    private:
+        double getAvg() const {
+            int sum = 0;
+            for (int i = 0; i < 3; i++){
+                sum += score[i];
+            }
+            return (double) sum / 3;
+        }
+    public:
+        string name;
+        string sid;
+        int* score;
+        
+        void print() const {
+            cout << "[" << sid << "] " << name << endl;
+            cout << "The Average score is " << getAvg() << endl;
+        }
+};
+
+int main()
+{
+    Student s1;
+    s1.score  = new int[3];
+    
+    cin >> s1.sid;
+
+    for (int i = 0; i < 3; i++)
+        cin >> s1.score[i];
+
+    cin.ignore();
+    getline(cin, s1.name);
+
+    s1.print();
+
+    delete[] s1.score;
+    return 0;
+}
