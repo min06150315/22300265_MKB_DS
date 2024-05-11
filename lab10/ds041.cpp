920a9aeda32382cdf6f9ffd7c0897b3aceb8aebc
min06150315 <min06150315@gmail.com>
2024-05-11 16:29:45 +0900

    Update ds041.cpp

--- lab10/ds041.cpp
+++ lab10/ds041.cpp
@@ -9,8 +9,9 @@ private:
 public:
     Node(int data) {     // constructor
         this->data = data;
-        this->next = NULL;
+        this->next = nullptr;
     }
+    friend class MyLinkedStack;
 };
 
 class MyLinkedStack {
@@ -23,50 +24,107 @@ public:
     void pop();
     int peek() const;
     int getNodeCnt() const;     // node의 개수를 가져옴
+    MyLinkedStack();
     ~MyLinkedStack();
     void printAll();        // stack을 pop순서로 출력(pop은 하지 않음)
 };
 
 void MyLinkedStack::initialize() {
-    int len = getNodeCnt();
-    for (int i = 0; i < len; i++) {
-        
+    // 현재 모든 스택을 비운다. (반복문)
+    while (!isEmpty()) {
+        //  1. temp node 생성
+        Node* temp_Node;
+
+        //  2. temp node에 stacktop 설정
+        temp_Node = stacktop;
+
+        //  3. stacktop에 stacktop의 next 설정
+        stacktop = stacktop->next;
+
+        //  4. temp node 삭제
+        delete temp_Node;
     }
 }
 
 bool MyLinkedStack::isEmpty() const {
-
+    return (stacktop == nullptr);
 }
 
-void MyLinkedStack::push(const int&) {
+void MyLinkedStack::push(const int& e) {
+    // 1. 새 Node 생성
+    Node* new_Node;
 
+    // 2. 새 Node에 new_element 값 설정
+    new_Node = new Node(e);
+
+    // 3. 새 Node의 링크에 Stacktop 추가
+    new_Node->next = stacktop;
+
+    // 4. Stacktop에 새 노드 주소 설정
+    stacktop = new_Node;
 }
 
 void MyLinkedStack::pop() {
+    // 1. isEmpty()이면 함수종료
+    if (isEmpty()) {
+        cout << "Stack is empty" << endl;
+        return;
+    }
+    // 2. temp node 생성
+    Node* temp_Node;
 
+    // 3. temp node에 stacktop 설정
+    temp_Node = stacktop;
+
+    // 4. stacktop에 stacktop의 next 설정
+    stacktop = stacktop->next;
+
+    // 5. temp node 삭제
+    delete temp_Node;
 }
 
 int MyLinkedStack::peek() const {
-
+    // 
+    if (isEmpty()) {
+        cout << "Stack is empty" << endl;
+        return -1;
+    }
+    return stacktop->data;
 }
 
 int MyLinkedStack::getNodeCnt() const {
-
+    Node* temp_Node = stacktop;
+    int count = 0;
+    while (temp_Node != nullptr) {
+        count++;
+        temp_Node = temp_Node->next;
+    }
+    return count;
 }
 
 MyLinkedStack::MyLinkedStack() {
-    initialize();
+    stacktop = nullptr;
 }
 
 MyLinkedStack::~MyLinkedStack() {
-
+    initialize();
 }
 
 void MyLinkedStack::printAll() {
-    int len = getNodeCnt();
-    for (int i = 0; i < len; i++) {
-        
+    if (isEmpty()) {
+        cout << "Stack is empty" << endl;
+        return;
     }
+    Node* temp = stacktop;
+
+    cout << temp->data ;
+    temp = temp->next;
+
+    while (temp != nullptr) {
+        cout << " -> " << temp->data ;
+        temp = temp->next;
+    }
+    cout << " (" << getNodeCnt() << " nodes)" << endl;
 }
 
 int main() {
@@ -88,9 +146,10 @@ int main() {
         } else if (menu == "clear") {   // 스택 초기화
             s1.initialize();
         } else if (menu == "q") {   // 종료
+            cout << "Bye!" << endl; 
             break;
         }
     }
 
-
+    return 0;
 }
