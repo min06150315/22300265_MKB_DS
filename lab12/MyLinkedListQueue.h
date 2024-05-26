770ecb828c85e12a2ad575942f831032f44e9573
min06150315 <min06150315@gmail.com>
2024-05-26 22:25:52 +0900

    Add LinkedListQueue Library

---
+++ lab12/MyLinkedListQueue.h
@@ -0,0 +1,103 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+    int data;
+    Node* next;
+    Node(int data) {
+        this->data = data;
+        this->next = NULL;
+    }
+    friend class MyLinkedListQueue;
+};
+
+class MyLinkedListQueue {
+    private:
+        Node* front;
+        Node* rear;
+    public:
+        MyLinkedListQueue();
+        ~MyLinkedListQueue();
+        void initialize();
+        void enqueue(const int&);
+        void dequeue();
+        bool isempty() const;
+        void Rear() const;
+        void Front() const;
+        void printAll() const;
+};
+MyLinkedListQueue::MyLinkedListQueue() {
+    front = NULL;
+    rear = NULL;
+}
+
+MyLinkedListQueue::~MyLinkedListQueue() {
+    initialize();
+}
+
+void MyLinkedListQueue::initialize() {
+    while (front != NULL) {
+        dequeue();
+    }
+    front = NULL;
+    rear = NULL;
+}
+
+void MyLinkedListQueue::enqueue(const int& e) {
+    Node* new_node = new Node(e);
+    if (front == NULL) {
+        front = new_node;
+        rear = new_node;
+    } else {
+        rear->next = new_node;
+        rear = new_node;
+    }
+}
+
+void MyLinkedListQueue::dequeue() {
+    if (isempty()) {
+        cout << "Queue is Empty." << endl;
+        return;
+    } else {
+        Node* ptr = front;
+        front = front->next;
+        if (front == NULL)
+            rear = NULL;
+        delete ptr;
+    }
+}
+
+bool MyLinkedListQueue::isempty() const {
+    return front == NULL;
+}
+
+void MyLinkedListQueue::Rear() const {
+    if (isempty())
+        cout << "Queue is empty." << endl;
+    else 
+        cout << "element at rear: " << rear->data << endl;
+}
+
+void MyLinkedListQueue::Front() const {
+    if (isempty())
+        cout << "Queue is empty." << endl;
+    else
+        cout << "element at front: " << front->data << endl;
+}
+
+void MyLinkedListQueue::printAll() const {
+    if (isempty()) {
+        cout << "Queue is empty." << endl;
+    } else {
+        Node* ptr = front;
+        
+        cout << ptr->data;
+        ptr = ptr->next;
+
+        while (ptr != NULL) {
+            cout  << " -> " << ptr->data;
+            ptr = ptr->next; 
+        }
+        cout << endl;
+    }
+}
