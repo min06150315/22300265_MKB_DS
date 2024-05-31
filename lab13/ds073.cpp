2aa02ccce9e3d9241703a8c0f449ec0c12629a8e
min06150315 <min06150315@gmail.com>
2024-05-31 12:01:32 +0900

    Update ds073.cpp

--- lab13/ds073.cpp
+++ lab13/ds073.cpp
@@ -32,23 +32,23 @@ int main() {
 
 void BubbleSortAscending(Element list[], int n) {
     for(int i = 0; i < n - 1; i++) {
-        for(int j = 0; j < n - i - 1; j++)
-            if(list[j] > list[j + 1])
-                swap(list[j], list[j + 1]);
 #ifdef DEBUGMODE
         PrintArray(list, n);
 #endif
+        for(int j = 0; j < n - i - 1; j++)
+            if(list[j] > list[j + 1])
+                swap(list[j], list[j + 1]);
     }
 }
 
 void BubbleSortDescending(Element list[], int n) {
     for(int i = 0; i < n - 1; i++) {
-        for(int j = 0; j < n - i - 1; j++)
-            if(list[j] < list[j + 1])
-                swap(list[j], list[j + 1]);
 #ifdef DEBUGMODE
         PrintArray(list, n);
 #endif
+        for(int j = 0; j < n - i - 1; j++)
+            if(list[j] < list[j + 1])
+                swap(list[j], list[j + 1]);
     }
 }
 
