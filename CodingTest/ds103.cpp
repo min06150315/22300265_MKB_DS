d1762465f922817fa29539d5095e4c87408f419d
min06150315 <min06150315@gmail.com>
2024-06-22 23:11:38 +0900

    Update ds103.cpp

--- CodingTest/ds103.cpp
+++ CodingTest/ds103.cpp
@@ -1,1 +1,22 @@
 // DS103. 상 받는 최소 점수는?
+#include <iostream>
+#include "./lib/Sorting.h"
+
+using namespace std;
+
+int main() {
+    int num, rank;
+    cin >> num >> rank;
+
+    int* list = new int[num];
+   
+    for (int i = 0; i < num; i++)
+        cin >> list[i]; 
+    
+    SelectionSort(list, num);
+
+    cout << list[rank - 1];
+
+    delete[] list;
+    return 0;
+}
