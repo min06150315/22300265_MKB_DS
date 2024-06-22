82a4e8a836be7244e74a159374358cf4cb32b70c
min06150315 <min06150315@gmail.com>
2024-06-22 23:11:32 +0900

    Update ds102.cpp

--- CodingTest/ds102.cpp
+++ CodingTest/ds102.cpp
@@ -1,1 +1,41 @@
 // DS102. 최댓값을 만들자.
+#include <iostream>
+#include "./lib/Sorting.h"
+using namespace std;
+
+typedef int Element;
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+    
+    int* deck1 = new int[n];
+    int* deck2 = new int[n];
+
+    for (int i = 0; i < n; i++)
+        cin >> deck1[i];
+    
+    for (int i = 0; i < n; i++)
+        cin >> deck2[i];
+
+    SelectionSort(deck1, n);
+    
+    for (int i = 0; i < k; i++) {
+        if (deck1[i] < deck2[i]) {
+            deck1[i] = deck2[i];
+        } else {
+            break;
+        }
+    }
+
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+        sum += deck1[i];
+    cout << sum << endl;
+
+
+    delete[] deck1;
+    delete[] deck2;
+
+    return 0;
+}
